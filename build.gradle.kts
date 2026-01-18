02ee4c57a2fe0f7b0aa8b7be3407732333d37141
AbdelrahmanAzab <abdelrahmanazab10@gmail.com>
2026-01-19 00:52:11 +0200

    build: Bump version code to 3 and version name to 2.1
    - **`build.gradle.kts`**: Incremented `versionCode` to 3 and updated `versionName` to 2.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.illa.cashvan"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
